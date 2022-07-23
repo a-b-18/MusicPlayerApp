ebf824fcdb2621c5e9975a7af9a8c5ec0134aa4e
Alex Beazley <alexbeazley18@outlook.com>
2022-07-23 14:14:12 +1000

    Plot audio file (time and freq), and plot live recording (time) working.

--- AudioPreviewApp/Form1.Designer.cs
+++ AudioPreviewApp/Form1.Designer.cs
@@ -47,6 +47,10 @@
             System.Windows.Forms.DataVisualization.Charting.Title title6 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ListBox_GreatestFrequency = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
@@ -138,11 +142,52 @@
             this.chart2.Titles.Add(title5);
             this.chart2.Titles.Add(title6);
             // 
+            // ListBox_GreatestFrequency
+            // 
+            this.ListBox_GreatestFrequency.FormattingEnabled = true;
+            this.ListBox_GreatestFrequency.Location = new System.Drawing.Point(781, 450);
+            this.ListBox_GreatestFrequency.Name = "ListBox_GreatestFrequency";
+            this.ListBox_GreatestFrequency.Size = new System.Drawing.Size(162, 186);
+            this.ListBox_GreatestFrequency.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(778, 434);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Frequency of Greatest Magnitude";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(799, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "record";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(799, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(965, 885);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ListBox_GreatestFrequency);
             this.Controls.Add(this.chart2);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
@@ -150,6 +195,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -157,6 +203,10 @@
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.ListBox ListBox_GreatestFrequency;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- AudioPreviewApp/Form1.cs
+++ AudioPreviewApp/Form1.cs
@@ -11,13 +11,14 @@ using MathNet.Numerics;
 using MathNet.Numerics.IntegralTransforms;
 using System.Numerics;
 using System.Windows.Forms.DataVisualization.Charting;
+using NAudio.Wave;
 
 namespace AudioPreviewApp
 {
     public partial class Form1 : Form
     {
         Complex[] samples = new Complex[1000];
-        int sampleRate = 2000;
+        int sampleRateOld = 2000;
         int magnitudeSecond;
         int magnitudeThird;
         double phaseSecond;
@@ -25,11 +26,11 @@ namespace AudioPreviewApp
 
         public Form1()
         {
-            InitializeComponent(); 
-            PlotWaveform();
+            InitializeComponent();
+            PlotAudioWaveform();
         }
 
-        public void PlotWaveform()
+        public void PlotTestWaveform()
         {
             var numSamples = samples.Length;
 
@@ -39,9 +40,9 @@ namespace AudioPreviewApp
             chart1.Series["Third Harmonic"].Points.Clear();
 
             // Generate fundamental, 2nd & 3rd harmonic waveforms using MathNet libraries
-            var first = Generate.Sinusoidal(numSamples, sampleRate, 60, 10.0, 0, 0);
-            var second = Generate.Sinusoidal(numSamples, sampleRate, 120, 5, 0, 0);
-            var third = Generate.Sinusoidal(numSamples, sampleRate, 180, 2, 0, 0);
+            var first = Generate.Sinusoidal(numSamples, sampleRateOld, 60, 10.0, 0, 0);
+            var second = Generate.Sinusoidal(numSamples, sampleRateOld, 120, 5, 0, 0);
+            var third = Generate.Sinusoidal(numSamples, sampleRateOld, 180, 2, 0, 0);
 
             // Add waveforms together to get composite waveforms
             for (int i = 0; i < numSamples; i++)
@@ -77,36 +78,26 @@ namespace AudioPreviewApp
             }
         }
 
-        public void PlotAudio()
+        public void PlotAudioWaveform()
         {
-            var numSamples = samples.Length;
+            double[] audio_samples;
+            var filePath = "C:\\Users\\alexb\\source\\repos\\MusicPlayerApp\\AudioPreviewApp\\Static\\C_synth.wav";
+            (audio_samples, sampleRateOld) = ReadWav(filePath);
+            samples = new Complex[audio_samples.Length];
+            var numSamples = audio_samples.Length;
 
             chart1.Series["Waveform"].Points.Clear();
-            chart1.Series["First Harmonic"].Points.Clear();
-            chart1.Series["Second Harmonic"].Points.Clear();
-            chart1.Series["Third Harmonic"].Points.Clear();
-
-            // Generate fundamental, 2nd & 3rd harmonic waveforms using MathNet libraries
-            var first = Generate.Sinusoidal(numSamples, sampleRate, 60, 10.0, 0, 0);
-            var second = Generate.Sinusoidal(numSamples, sampleRate, 120, 5, 0, 0);
-            var third = Generate.Sinusoidal(numSamples, sampleRate, 180, 2, 0, 0);
 
             // Add waveforms together to get composite waveforms
             for (int i = 0; i < numSamples; i++)
             {
-                samples[i] = new Complex(first[i] + second[i] + third[i], 0);
-
-                if (i > numSamples / 10)
-                    continue;
-
-                chart1.Series["Waveform"].Points.AddXY(i, samples[i].Real);
-                chart1.Series["First Harmonic"].Points.AddXY(i, first[i]);
-                chart1.Series["Second Harmonic"].Points.AddXY(i, second[i]);
-                chart1.Series["Third Harmonic"].Points.AddXY(i, third[i]);
+                samples[i] = new Complex(audio_samples[i], 0);
+                if (i % 100 == 0)
+                    chart1.Series["Waveform"].Points.AddXY(i, samples[i].Real);
             }
 
             // Compute fourier transform
-            Complex[] fourierSeries = new Complex[1000];
+            Complex[] fourierSeries = new Complex[audio_samples.Length];
             samples.CopyTo(fourierSeries, 0);
             Fourier.Forward(fourierSeries, FourierOptions.NoScaling);
 
@@ -114,7 +105,7 @@ namespace AudioPreviewApp
             var frequencyList = new List<int>();
 
             // Add waveforms together to get composite waveforms
-            for (int i = 0; i < numSamples / 10; i++)
+            for (int i = 0; i < numSamples / 40; i++)
             {
                 var mag = (2.0 / numSamples) * (fourierSeries[i].Magnitude);
                 chart2.Series["Waveform"].Points.AddXY(i * 2, mag);
@@ -123,7 +114,90 @@ namespace AudioPreviewApp
                 if (mag > 0.2)
                     frequencyList.Add(i * 2);
             }
+
+            // Add
+            frequencyList.Sort();
+            frequencyList.Reverse();
+            frequencyList.Take(5).ToList().ForEach(freq => ListBox_GreatestFrequency.Items.Add(freq));
+            ListBox_GreatestFrequency.Refresh();
+        }
+        static (double[] audio, int sampleRate) ReadWav(string filePath)
+        {
+            using (var afr = new NAudio.Wave.AudioFileReader(filePath))
+            {
+                int sampleRate = afr.WaveFormat.SampleRate;
+                int sampleCount = (int)(afr.Length / afr.WaveFormat.BitsPerSample / 8);
+                int channelCount = afr.WaveFormat.Channels;
+                var audio = new List<double>(sampleCount);
+                var buffer = new float[sampleRate * channelCount];
+                int samplesRead = 0;
+                while ((samplesRead = afr.Read(buffer, 0, buffer.Length)) > 0)
+                    audio.AddRange(buffer.Take(samplesRead).Select(x => (double)x));
+                return (audio.Take(10000).ToArray(), sampleRate);
+            }
         }
 
+
+
+        WaveIn waveIn;
+        WaveFileWriter waveWriter;
+        private const int sampleRate = 44100;
+        public NAudio.Wave.BufferedWaveProvider waveBuffer = null; // clears buffer 
+        ISampleProvider samplesProvider;
+        private void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveWriter == null) return;
+
+            // Prepare buffer
+            waveBuffer.AddSamples(e.Buffer, 0, e.BytesRecorded);
+            waveBuffer.DiscardOnBufferOverflow = true;
+            var bytesPerFrame = waveIn.WaveFormat.BitsPerSample / 8
+                              * waveIn.WaveFormat.Channels;
+            var bufferedFrames = waveBuffer.BufferedBytes / bytesPerFrame;
+            var frames = new float[bufferedFrames];
+            samplesProvider.Read(frames, 0, bufferedFrames);
+
+            // Plot buffer on graph
+            chart1.Series["Waveform"].Points.Clear();
+            foreach (var frame in frames.Select((_data, _index) => (_data = _data, _index = _index))
+                .Where(frame => frame.Item2 % 100 == 0))
+            {
+                chart1.Series["Waveform"].Points.AddXY(frame.Item2, frame.Item1);
+            }
+
+            waveWriter.Write(e.Buffer, 0, e.BytesRecorded);
+            waveWriter.Flush();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            waveIn = new WaveIn();
+            waveIn.DeviceNumber = 0;
+            string outputFilename = @"C:/Temp/demo.wav";
+            waveIn.WaveFormat = new WaveFormat(sampleRate, WaveIn.GetCapabilities(waveIn.DeviceNumber).Channels);
+
+            waveBuffer = new NAudio.Wave.BufferedWaveProvider(waveIn.WaveFormat); // initializes buffer
+            samplesProvider = waveBuffer.ToSampleProvider();
+
+
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(waveIn_DataAvailable);
+            waveWriter = new WaveFileWriter(outputFilename, waveIn.WaveFormat);
+            waveIn.StartRecording();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (waveIn != null)
+            {
+                waveIn.StopRecording();
+                waveIn.Dispose();
+                waveIn = null;
+            }
+            if (waveWriter != null)
+            {
+                waveWriter.Dispose();
+                waveWriter = null;
+            }
+        }
     }
 }
